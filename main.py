f00d943799804f1decca7875fdb2b52f4879e9e5
4wardAerial <4lucas.h.bertanha8@gmail.com>
2026-05-17 12:15:43 -0300

    file coping from device to local

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 from time import sleep
 from pathlib import Path
 from pytubefix import Playlist
+from shutil import copy2
 
 from manager import download_playlist, sync_playlist
 
-TEMP_PATH = '/temp'  # Path to temp folder 
+TEMP_PATH = '/home/aerial/Coding/Python/playlist_loader/temp'  # Path to temp folder 
 DEVICE_PATH = '/run/user/1000/gvfs/mtp:host=motorola_moto_g56_5G_ZF5257PRVK/SD_MUSIC/Music'  # Path to Device
 PLAYLISTS_URLS : list[str] = [
     'https://www.youtube.com/playlist?list=PLKhMBl2bi_P8E7ajBeqDttWEvhMW4beFv',  # The Best of Youtube
@@ -16,21 +17,25 @@ PLAYLISTS_URLS : list[str] = [
 ]
 
 
-def create_dir(DEVICE_OUTPUT_PATH : Path):
+def create_dir(output_path : Path):
     try:
-        os.mkdir(DEVICE_OUTPUT_PATH)
-        print(f"Directory '{DEVICE_OUTPUT_PATH}' created successfully.")
+        os.mkdir(output_path)
+        print(f"Directory '{output_path}' created successfully.")
     except FileExistsError:
-        print(f"Directory '{DEVICE_OUTPUT_PATH}' already exists.")
+        print(f"Directory '{output_path}' already exists.")
     except PermissionError:
-        print(f"[ERROR] Permission denied: Unable to create '{DEVICE_OUTPUT_PATH}'")
+        print(f"[ERROR] Permission denied: Unable to create '{output_path}'")
     except FileNotFoundError:
         raise FileNotFoundError
  
 
 if __name__ == '__main__':
     print('\nYouTube playlist downloader -------------------------made by 4wardAerial')
-    # Checks if device is a Mobile (needs temp files) or an USB 
+    
+    # Checks if device exists, if it is a Mobile (needs temp files) or an USB 
+    if not Path(DEVICE_PATH).exists():
+        print("\nNo device was found.")
+        exit(1)
     if 'gvfs' in DEVICE_PATH or 'mtp:host' in DEVICE_PATH:
         IS_MOBILE : bool = True
         print("\nSearching for MOBILE device.")
@@ -54,27 +59,37 @@ if __name__ == '__main__':
             if IS_MOBILE:
                 LOCAL_OUTPUT_PATH : str = f'{TEMP_PATH}/{dir}'
                 create_dir(LOCAL_OUTPUT_PATH)  # Creates directory locally with the playlist's name
+            else:
+                # If device is an USB, it does not need to create temp files, so the the local output
+                # can be set as the actual output to be redundant
+                LOCAL_OUTPUT_PATH = DEVICE_OUTPUT_PATH
 
-            urls_txt = Path(f'{DEVICE_OUTPUT_PATH}/urls.txt')
+            device_urls_txt = Path(f'{DEVICE_OUTPUT_PATH}/urls.txt')
+            local_urls_txt = Path(f'{LOCAL_OUTPUT_PATH}/urls.txt')
             urls_dict : dict = {}
 
-            if not os.path.exists(urls_txt):
-                print(f"File '{urls_txt}' created successfully.")
-                open(urls_txt, 'w').close()
-            else:
-                print(f"File '{urls_txt}' already exists.")
-                with open(urls_txt, 'r+', encoding='utf-8') as urltxt:
+            if os.path.exists(device_urls_txt):
+                if IS_MOBILE:
+                    print(f"File '{device_urls_txt}' found on Mobile, copied to local.")
+                    copy2(device_urls_txt, local_urls_txt)
+
+                # Reminder taht local_urls_txt is the same as device_urls_txt if device is an USB
+                with open(local_urls_txt, 'r+', encoding='utf-8') as urltxt:
                     lines = urltxt.readlines()
                     for line in lines:
                         url, title = line.split(sep=',', maxsplit=1)
                         urls_dict[url] = [title, 0]  # converts the lines to a 'url : (title, counter)' dictionary
-            
+            else:
+                print(f"File '{device_urls_txt}' not found, creating one locally.")
+                open(local_urls_txt, 'w').close()
+
+            exit(1)
             logs_txt = Path(f'{DEVICE_OUTPUT_PATH}/logs.txt')
             logs_dict : dict = {}
             open(logs_txt, 'w', encoding='utf-8').close()
             print(f"File '{logs_txt}' created successfully.\n")
             
-            download_playlist(p, urls_dict, logs_dict, urls_txt, logs_txt, DEVICE_OUTPUT_PATH)
+            download_playlist(p, urls_dict, logs_dict, device_urls_txt, logs_txt, DEVICE_OUTPUT_PATH)
             with open(logs_txt, 'r', encoding='utf-8') as logtxt:
                 lines = logtxt.readlines()
             print(f'\nDownloaded Playlist {p.title} with {len(lines)}/{p.length} skips.')
@@ -82,7 +97,7 @@ if __name__ == '__main__':
             sleep(1)
 
             if mode == 1:
-                deleted : int = sync_playlist(urls_dict, logs_dict, urls_txt, logs_txt, DEVICE_OUTPUT_PATH)
+                deleted : int = sync_playlist(urls_dict, logs_dict, device_urls_txt, logs_txt, DEVICE_OUTPUT_PATH)
                 print(f'\nSynced Playlist {p.title} with {deleted} deletions')
                 print("------------------------------------------------------------------------")
                 sleep(1)
@@ -94,5 +109,5 @@ if __name__ == '__main__':
         print('\nAll playlists updated successfully.\n')
     except KeyboardInterrupt as e:
         print('\nProgram forcefully ended.')
-    except FileNotFoundError as e:
-        print(f'\n[ERROR] USB not found. Error: {e}\n')
+    # except Exception as e:
+    #     print(f'\n[ERROR] {e}\n')
